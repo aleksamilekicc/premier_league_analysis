25a0256b8681c1d3003388b3035e9ed554ed79c9
aleksamilekicc <aleksamilekic@gmail.com>
2026-07-10 14:36:43 +0200

    Add goals analysis

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -12,3 +12,17 @@ select tim, COUNT(*) as broj_pobeda
 from pobede
 group by tim
 order by broj_pobeda DESC
+
+"Koji tim je dao najvise golova ukupno"
+with golovi as (select "HomeTeam" as tim,"FullTimeHomeGoals" as golovi
+from utakmice
+
+union all
+
+select "AwayTeam" as tim,"FullTimeAwayGoals" as golovi
+from utakmice
+)
+
+select tim, SUM(golovi) as ukupno_golova
+from golovi
+group by tim order by ukupno_golova DESC
